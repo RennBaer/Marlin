6f3a643ae4d41f412baeceee1c45334582149081
RennBaer <36121289+RennBaer@users.noreply.github.com>
2021-02-02 18:38:24 -0500

    Edited bed travel limits

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1166,14 +1166,14 @@
 
 // The size of the print bed
 #define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define Y_BED_SIZE 225
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -10
+#define Y_MIN_POS 5
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS 225 //Y_BED_SIZE - 10
+#define Y_MAX_POS 230
 #define Z_MAX_POS 240
 
 /**
